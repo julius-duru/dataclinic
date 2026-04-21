bd9a1a214b445323d492569d255738147cf55db6
Duru Julius <30971047+julius-duru@users.noreply.github.com>
2026-04-21 10:20:53 +0100

    Add files via upload
    Signed-off-by: Duru Julius <30971047+julius-duru@users.noreply.github.com>

--- blog/categories.py
+++ blog/categories.py
@@ -1,13 +1,11 @@
 from blog.database import mysql_databases, mysql_vs_postgressql
 from blog.machine_learning import ml_data_pipeline, ml_concept, choosing_ml_model, model_retraining_in_production, machine_learning_workflow, mlops_pipeline
-from blog.misc import storytelling_with_data, end_to_end_datascience, data_preprocessing, clean_other_data_sources, domain_knowledge,dashboard_kpi_design, power_bi_report, mlops_workflow, data_governance
+from blog.misc import storytelling_with_data, end_to_end_datascience, data_preprocessing, clean_other_data_sources, domain_knowledge,dashboard_kpi_design, power_bi_report, mlops_workflow, data_governance, mlops_frameworks, securing_model
 from blog.data_engineering import data_cleanig_sql, missing_values, model_lifecycle_management
 from blog.programming_tools import datascience_toolkit
 from blog.data_visualization import power_bi, power_bi_visualization_tools
-from blog.misc import mlops_frameworks, securing_model, data_story
 from blog.deep_learning import deep_learning_guide, cnn_and_rnn, ann_end_to_end, ann_cnn_rnn_lstm
 from blog.docker import docker_setup, docker_guide, flask_mysql_in_docker, flask_postgresql_in_docker
-
 
 CATEGORIES = {
     "Database": [
